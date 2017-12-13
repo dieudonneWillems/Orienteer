eee9e0b46b49ea70c8987b1d899c9ee040e7e84a
Dieudonné Willems <don.willems@wur.nl>
2017-12-13 21:56:01 +0100

    Initial Commit

---
+++ Orienteer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Orienteer
+//
+//  Created by Don Willems on 13/12/2017.
+//  Copyright © 2017 lapsedpacifist. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Orienteer/Document.swift
@@ -0,0 +1,28 @@
+//
+//  Document.swift
+//  Orienteer
+//
+//  Created by Don Willems on 13/12/2017.
+//  Copyright © 2017 lapsedpacifist. All rights reserved.
+//
+
+import Cocoa
+
+class Document: NSPersistentDocument {
+
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override class var autosavesInPlace: Bool {
+        return true
+    }
+
+    override var windowNibName: NSNib.Name? {
+        // Returns the nib file name of the document
+        // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
+        return NSNib.Name("Document")
+    }
+
+}
